fb5b5b93b26bfbc8856036c14aca11575710fd05
hamzah-syed <hamzah.syed17@gmail.com>
2021-05-15 16:53:41 +0500

    function code updated

--- src/lambda/hello.ts
+++ src/lambda/hello.ts
@@ -1,8 +1,11 @@
-exports.handler = () => {
+exports.handler = async () => {
   try {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello World" }),
+      body: JSON.stringify({ message: `Hello World` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
